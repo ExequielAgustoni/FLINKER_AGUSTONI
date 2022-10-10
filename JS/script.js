c859dd5cc38236a7b01b3d50ff035796db34aadd
Exequiel Agustoni <Exequiel_Agustoni12@hotmail.com>
2022-10-10 02:14:59 -0300

    Agregue objetos y arrays

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,37 @@
-let nombreProductoA = 'Xbox X'
-let precioProductoA = 1200
-let stockProductoA = 150
 
-let nombreProductoB = 'Play 4'
-let precioProductoB = 1400
-let stockProductoB = 300
+function Producto(nombre,precio,stock){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+}
 
-let nombreProductoC = 'Laptop Asus'
-let precioProductoC = 1100
-let stockProductoC = 150
+let productoA = new Producto("Xbox X", 1200, 150)
+let productoB = new Producto("Play 4", 1400, 300)
+let productoC = new Producto("Laptop Asus", 1100, 150)
 
 let precioTotal = 0
+
+let listaProductos = [productoA, productoB, productoC]
+
+let nombresAlerta = []
+
+for(const nombres of listaProductos){
+    nombresAlerta.push(nombres.nombre)
+}
 
 function saludo(ingresoPagina, cierreCompra){
 
     alert(ingresoPagina + "Flinker" + cierreCompra)
 }
 
+let precioMenor = listaProductos.filter ((precioProductos) => precioProductos.precio <1300)
+
+console.log (precioMenor)
+
 saludo("Bienvenido a ","")
 
 
-alert("Estos son nuestros productos: \n - Xbox X\n - Play 4\n - Laptop Asus")
+alert("Estos son nuestros productos: \n - " + nombresAlerta.join("\n - "))
 
 
 let opcion = alert ("Ingrese que es lo que quiere comprar o escriba ESC para salir")
@@ -29,22 +40,33 @@ while(opcion != "ESC"){
 
     opcion = prompt ("Ingrese que producto quiere comprar: \n - Xbox X\n - Play 4\n - Laptop Asus\n - ESC ")
     
-    if(opcion == 'Xbox X'){
-        let cantidadProductoXbox = parseInt(prompt("ingrese que cantidad de " + nombreProductoA + " desea comprar:"))
-        if(cantidadProductoXbox <= stockProductoA){
-            precioTotal = precioTotal + (cantidadProductoXbox * precioProductoA)
+    if(opcion.toUpperCase() == 'XBOX X'){
+        let cantidadProductoXbox = parseInt(prompt("ingrese que cantidad de " + productoA.nombre + " desea comprar:"))
+        if(cantidadProductoXbox <= productoA.stock){
+            precioTotal = precioTotal + (cantidadProductoXbox * productoA.precio)
         }
         else{
-            alert("Actualmente tenemos " + stockProductoA + " unidades de este producto")
+            alert("Actualmente tenemos " + productoA.stock + " unidades de este producto")
         }
     }
-    else if(opcion == 'Play 4'){
-        let cantidadProductoPlay = parseInt(prompt("ingrese que cantidad de " + nombreProductoB + " desea comprar:"))
-        precioTotal = precioTotal + (cantidadProductoPlay * precioProductoB)
-    }
-    else if(opcion == 'Laptop Asus'){
-        let cantidadProductoLaptop = parseInt(prompt("ingrese que cantidad de " + nombreProductoC + " desea comprar:"))
-        precioTotal = precioTotal + (cantidadProductoLaptop * precioProductoC)
+    else if(opcion.toUpperCase() == 'PLAY 4'){
+        let cantidadProductoPlay = parseInt(prompt("ingrese que cantidad de " + productoB.nombre + " desea comprar:"))
+        if(cantidadProductoPlay <= productoB.stock){
+        precioTotal = precioTotal + (cantidadProductoPlay * productoB.precio)
+        }
+        else{
+            alert("Actualmente tenemos " + productoB.stock + " unidades de este producto")
+        }
+    } 
+      
+    else if(opcion.toUpperCase() == 'LAPTOP ASUS'){
+        let cantidadProductoLaptop = parseInt(prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar:"))
+        if(cantidadProductoLaptop <= productoC.stock){
+        precioTotal = precioTotal + (cantidadProductoLaptop * productoC.precio)
+        }
+        else{
+            alert("Actualmente tenemos " + productoC.stock + " unidades de este producto")
+        }
     }        
 }
 
